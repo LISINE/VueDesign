ee0694726cd03b93c4ad8a12baa9ae088abd1212
pastJourney <1692904709@qq.com>
2019-10-29 19:55:40 +0800

    adddeatail

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import store from './store'
 //这样就可以用this.axios在其他文件中随意使用axios
 import axios from 'axios'  
 Vue.prototype.axios = axios;
+//引入echarts
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
 new Vue({
   router,
   store,
